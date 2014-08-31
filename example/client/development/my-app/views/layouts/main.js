7c284047a7e0319bd461509167a0de5ed301a5ed
vvorobioff <i_am_vital@mail.ru>
2014-08-31 09:42:33 +0400

    +

--- example/client/development/my-app/views/layouts/main.js
+++ example/client/development/my-app/views/layouts/main.js
@@ -1,13 +1,17 @@
 benderDefine('Views:Layouts:Main', function (app) {
+	this.use('Views:Common:Base');
 	this.configure({
 		template: 'Views:Layouts:Main'
 	});
-	return app.Views.Base.extend({
-		initialize: function () {
-			this.render();
-		},
-		render: function () {
-			this.$el.html(this.template.render({page: app.Views.currentPage}))
-		}
-	});
+	return function () {
+		return app.Views.get('Views:Common:Base').extend({
+			initialize: function (options) {
+				this.template = options.template;
+			},
+			render: function () {
+				this.$el.html(this.template.render({page: app.Views.currentPage}));
+				return this;
+			}
+		});
+	};
 });
