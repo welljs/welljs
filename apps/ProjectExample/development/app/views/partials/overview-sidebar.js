8856771d3fd3b443768b54e7ff8215dcb7179e92
vvorobioff <i_am_vital@mail.ru>
2014-10-07 21:38:48 +0400

    project struct refactored

--- apps/ProjectExample/development/app/views/partials/overview-sidebar.js
+++
@@ -1,6 +0,0 @@
-wellDefine('Views:Partials:OverviewSidebar', function (app) {
-	this.use('Views:Common:Sidebar');
-	this.exports(function () {
-		return app.Views.get('Views:Common:Sidebar').extend({});
-	});
-});
