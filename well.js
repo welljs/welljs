da47d7e6cc3574d4eb5496b202e1e90f3cf14f7b
vvorobioff <i_am_vital@mail.ru>
2014-11-14 00:47:30 +0300

    license added

--- well.js
+++ well.js
@@ -1,6 +1,14 @@
 (function(){
-	 'use strict'; 
- 	var app;
+	'use strict'; 
+ /*
+ The MIT License (MIT)
+ Copyright (c) 2014 vitalii.vorobioff@gmail.com
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.	var app;
+ */
+
+	var app;
 	var modulesController;
 	var noop = function(){};
 	//a bit of functionality borrowed from Underscore.js
@@ -383,10 +391,24 @@
 			}, this);
 		},
 
+		_exist: function (a) {
+			return !!_.find(this.names, function (m) {
+				return a.name === m.name;
+			});
+		},
+
+		_findMissing: function (mods) {
+			var r = [];
+			_.each(mods, function (m) {
+				!this._exist(m) && !modulesController.exist(m.name) && r.push(m);
+			}, this);
+			return r;
+		},
+
 		onModuleDefined: function (module) {
 			if (this.isModuleFromThisQueue(module.name)) {
 				this.handleModule(module);
-				var deps = modulesController.findMissing(module.getDeps());
+				var deps = this._findMissing(module.getDeps());
 				deps.length 
 					? this.enqueue(deps)
 					: (this.isQueueEmpty() && this.complete());
